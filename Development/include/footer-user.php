2bb34aa547eb83be648790a9f798cd12ce434171
Gabriel <gabrielmanchmail@gmail.com>
2016-07-14 18:31:01 +0500

    git fixings

--- Development/include/footer-user.php
+++
@@ -1,9 +0,0 @@
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="bootstrap/js/jquery-1.9.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-</body>
-
-</html>
